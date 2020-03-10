8db8065272511654815faf60d86e96867fb89db5
Jackson Oppenheim <jacksonop@gmail.com>
2020-03-10 13:18:06 -0500

    finished logic for attendee registration

--- src/components/Registration/RegistrationForm.tsx
+++ src/components/Registration/RegistrationForm.tsx
@@ -3,48 +3,45 @@ import React, { useReducer } from "react";
 import { Row, Col, Button } from "react-bootstrap";
 
 import FormInput from "../Forms/FormInput";
+import AttendeeForm from "./AttendeeForm";
 
 interface Props {}
 
-interface Attendee {
-  fName: string;
-  lName: string;
-  email: string;
+export class Attendee {
+  constructor(public i: number, public fName: string, public lName: string, public email: string) {}
 }
 
 interface State {
   attendees: Attendee[];
   numOfAttendees: number;
-  numberTracker: number[];
 }
 
-type Action =
-  | { type: "attendees"; payload: string }
-  | { type: "numOfAttendees"; payload: string }
-  | { type: "numberTracker"; payload: string };
+type Action = { type: "attendees"; payload: Attendee } | { type: "numOfAttendees"; payload: string };
 
 const reducer = (state: State, action: Action) => {
   switch (action.type) {
     case "attendees":
-      return { ...state };
+      const index = action.payload.i;
+      let tempArr: Attendee[] = state.attendees;
+      tempArr[index] = action.payload;
+      return { ...state, attendees: tempArr };
     case "numOfAttendees":
-      const newNumber = parseInt(action.payload);
-      state.numberTracker = [];
-      for (let i = 0; i < newNumber; i++) {
-        state.numberTracker.push(i);
+      if (parseInt(action.payload) < 0) {
+        return { ...state, numOfAttendees: 0 };
       }
-      return { ...state, numOfAttendees: newNumber };
-    case "numberTracker":
-      return { ...state };
+      const newNumber = parseInt(action.payload);
+      let temp: Attendee[] = [];
+      for (let i = 0; i < newNumber; i++) {
+        temp.push(new Attendee(i, "", "", ""));
+      }
+      return { ...state, numOfAttendees: newNumber, attendees: temp };
     default:
       return { ...state };
   }
 };
 
 const RegistrationForm: React.FC<Props> = () => {
-  const [state, dispatch] = useReducer(reducer, { attendees: [], numOfAttendees: 0, numberTracker: [] });
-
-  console.log(state);
+  const [state, dispatch] = useReducer(reducer, { attendees: [], numOfAttendees: 0 });
   return (
     <>
       <Row>
@@ -55,11 +52,18 @@ const RegistrationForm: React.FC<Props> = () => {
       <Row>
         <Col md={2}>
           <FormInput title="Number of attendees" cb={dispatch} action="numOfAttendees" type="number" placeholder="0" />
-          <p>{state.numOfAttendees}</p>
-
-          {state.numberTracker.map(n => (
-            <p>input</p>
+        </Col>
+      </Row>
+      <Row className="mt-5">
+        <Col>
+          {state.attendees.map(attendee => (
+            <AttendeeForm key={attendee.i} attendeeInfo={attendee} cb={dispatch} action="attendees" />
           ))}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button onClick={() => console.log(state)}>Submit</Button>
         </Col>
       </Row>
     </>
