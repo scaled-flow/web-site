6a5f6b8e8cf3b68e11e2db029ea18cc9bd7efddc
Jackson Oppenheim <jacksonop@gmail.com>
2020-03-21 11:49:39 -0500

    added paypal button, config for working with typescript

--- src/components/Registration/PaymentOptions.tsx
+++ src/components/Registration/PaymentOptions.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { PayPalButton } from "react-paypal-button-v2";
+
 import { Transaction } from "../../pages/client/ClassRegistrationPage";
 
 interface Props {
@@ -10,14 +12,22 @@ const PaymentOptions: React.FC<Props> = ({ transaction }) => {
   return (
     <div className="reg-row text-center">
       <h2 className="reg-header">Payment Options</h2>
-      <button
-        className="submit-btn btn-wide"
-        onClick={() => {
-          console.log(transaction);
+      <PayPalButton
+        amount={transaction.totalPrice.toString()}
+        // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
+        // @ts-ignore
+        onSuccess={(details, data) => {
+          alert("Transaction completed by " + details.payer.name.given_name);
+
+          // OPTIONAL: Call your server to save the transaction
+          return fetch("/paypal-transaction-complete", {
+            method: "post",
+            body: JSON.stringify({
+              orderID: data.orderID
+            })
+          });
         }}
-      >
-        Submit Registration
-      </button>
+      />{" "}
     </div>
   );
 };
