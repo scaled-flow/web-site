263e47b196f3997bd311433f3cc6376aa5b0f49c
Jackson Oppenheim <jacksonop@gmail.com>
2020-03-25 10:30:31 -0500

    added alt text

--- src/components/Header/TrainingHeader.tsx
+++ src/components/Header/TrainingHeader.tsx
@@ -22,7 +22,11 @@ const TrainingHeader: React.FC<Props> = ({ title, description, type }) => {
               <h6>{description}</h6>
             </Col>
             <Col md={4} className="d-sm-none d-md-block header-content">
-              <Image fluid src={type === "SA" ? keystone : less} />
+              <Image
+                fluid
+                alt={type === "SA" ? "Scaled Agile Flow" : "LeSS Framework"}
+                src={type === "SA" ? keystone : less}
+              />
             </Col>
           </Row>
         </Container>
